26e06c0ee10878c0aeb3b9c942359c8e5bdabf23
Tim Bodiford <tcbodiford@gmail.com>
2019-07-20 13:43:29 -0400

    created getUsers and getUser models and controller

---
+++ controllers/user.js
@@ -0,0 +1,63 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const userApi = require('../models/user.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const userRouter = express.Router()
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+/* Step 5
+ *
+ * TODO: delete this handler; it's just a sample
+ */ 
+userRouter.get('/', (req, res) => {
+  userApi.getAllUsers()
+  .then((users) => {
+    res.json(users)
+  })
+})
+
+userRouter.get('/:userId', (req, res) => {
+  userApi.getAllUsers(req.params.userId)
+  .then((user)=> {
+    res.json(user)
+  }) 
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+/* Step 6
+ *
+ * Export the router from the file.
+ *
+ */
+module.exports = {
+  userRouter
+}
---
+++ models/user.js
@@ -0,0 +1,73 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 1 alternative
+ *
+ * TODO: make a global variable to act as an in memory database. 
+ * NOTE: doing this WILL NOT persist your data and you will loose
+ * your data once you stop running your server.
+ *
+ */
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  pointsBalance: Number,
+  userName: String,
+  address: String,
+  city: String,
+  state: String,
+  zip: String
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const UserCollection = mongoose.model('User', UserSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getAllUsers() {
+  return UserCollection.find()
+}
+
+function getUser(userId) {
+  return UserCollection.findById(userId)
+}
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+getAllUsers,
+getUser
+}
