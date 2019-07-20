36edd8a4e9e3120ca3a5338e443d0d8d8784d8d8
Tim Bodiford <tcbodiford@gmail.com>
2019-07-20 15:53:50 -0400

    added product controllers - untested

---
+++ controllers/product.js
@@ -0,0 +1,87 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const productApi = require('../models/product.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const productRouter = express.Router()
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+/* Step 5
+ *
+ * TODO: delete this handler; it's just a sample
+ */ 
+productRouter.get('/', (req, res) => {
+  productApi.getAllProducts()
+  .then((users) => {
+    res.json(users)
+  })
+})
+
+productRouter.get('/:productId', (req, res) => {
+  productApi.getProduct(req.params.productId)
+  .then((product)=> {
+    res.json(product)
+  }) 
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+productRouter.post('/', (req, res) => {
+  productApi.createProduct(req.body)
+  .then((product) => {
+    res.json(product)
+  })
+})
+
+productRouter.put('/:productId', (req, res) => {
+  productApi.updateProduct(req.params.productId, req.body)
+  .then((updatedProduct) => {
+    res.json(updatedProduct)
+  })
+})
+
+productRouter.delete('/:productId', (req, res) => {
+  productApi.deletePRoduct(req.params.productId)
+  .then((product) => {
+    res.json(product)
+  })
+})
+
+
+
+
+/* Step 6
+ *
+ * Export the router from the file.
+ *
+ */
+module.exports = {
+  productRouter
+}
---
+++ models/product.js
@@ -0,0 +1,85 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 1 alternative
+ *
+ * TODO: make a global variable to act as an in memory database. 
+ * NOTE: doing this WILL NOT persist your data and you will loose
+ * your data once you stop running your server.
+ *
+ */
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const ProductSchema = new mongoose.Schema({
+  productName: String,
+  productDescription: String,
+  pointsRequired: String,
+  productImage: {
+    link: String,
+    default: "https://picsum.photos/200"
+  },
+  category: String,
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const ProductCollection = mongoose.model('Product', ProductSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getAllProducts() {
+  return ProductCollection.find()
+}
+
+function getProduct(productId) {
+  return ProductCollection.findById(productId)
+}
+
+function createProduct(productObject) {
+  return ProductCollection.create(productObject)
+}
+
+function updateProduct(productId, updatedProduct) {
+  return ProductCollection.findByIdAndUpdate(productId, updatedProduct, { new: true })
+}
+
+function deleteProduct(productId) {
+  return ProductCollection.findByIdAndDelete(productId)
+}
+
+
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct
+}
