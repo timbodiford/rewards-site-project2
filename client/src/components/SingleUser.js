8e3a1e3430b93dfbcbd07b6d746216aa5c298132
Tim Bodiford <tcbodiford@gmail.com>
2019-07-23 10:22:04 -0400

    able to edit user - tested

--- client/src/components/SingleUser.js
+++ client/src/components/SingleUser.js
@@ -6,7 +6,8 @@ import { Redirect, Link } from 'react-router-dom'
 export default class SingleUser extends Component {
 
     state = {
-        user: {}
+        user: {},
+        isEditFormDisplayed: false
     }
 
     componentDidMount() {
@@ -14,7 +15,33 @@ export default class SingleUser extends Component {
             .then((res) => {
                 this.setState({ user: res.data })
             })
-            console.log(this.state.user)
+        console.log(this.state.user)
+    }
+
+    handleToggleEditForm = () => {
+        this.setState((state) => {
+            return { isEditFormDisplayed: !state.isEditFormDisplayed }
+        })
+    }
+
+    handleEdit = (event) => {
+        event.preventDefault()
+
+        axios.put(`/api/users/${this.state.user._id}`, this.state.user)
+            .then((res) => {
+                this.setState({
+                    user: res.data,
+                    isEditFormDisplayed: false
+                })
+
+            })
+    }
+
+    handleInputEdit = (event) => {
+        const copiedUser = { ...this.state.user }
+        copiedUser[event.target.name] = event.target.value
+
+        this.setState({ user: copiedUser})
     }
 
 
@@ -24,19 +51,122 @@ export default class SingleUser extends Component {
 
         }
         return (
-            <div>
-                <p>Individual User should show here:</p>
-                
-                <h5>{this.state.user.firstName}</h5>
-                <h5>{this.state.user.lastName}</h5>
+            this.state.isEditFormDisplayed
+                ?
+            
+                <form onSubmit={this.handleEdit}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.firstName}
+                    />
+
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.lastName}
+                    />
+
+                    <label htmlFor='email'>Email Address</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.email}
+                    />
+
+                    <label htmlFor='isAdmin'>Admin User?</label>
+                    <input
+                        type="text"
+                        id="isAdmin"
+                        name="isAdmin"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.isAdmin}
+                    />
+
+                    <label htmlFor='pointsBalance'>Points pointsBalance</label>
+                    <input
+                        type="text"
+                        id="pointsBalance"
+                        name="pointsBalance"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.pointsBalance}
+                    />
+
+                    <label htmlFor='userName'>User Name</label>
+                    <input
+                        type="text"
+                        id="userName"
+                        name="userName"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.userName}
+                    />
+
+                    <label htmlFor='address'>Address</label>
+                    <input
+                        type="text"
+                        id="address"
+                        name="address"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.address}
+                    />
+
+                    <label htmlFor='city'>City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.city}
+                    />
+
+                    <label htmlFor='state'>state</label>
+                    <input
+                        type="text"
+                        id="state"
+                        name="state"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.state}
+                    />
+
+                    <label htmlFor='zip'>Zip Code</label>
+                    <input
+                        type="text"
+                        id="zip"
+                        name="zip"
+                        onChange={this.handleInputEdit}
+                        value={this.state.user.zip}
+                    />
+                    <input type="submit" value="Save Changes" />
+
+                </form>
+
+
+
+            :
+                <div>
+                    <p>Individual User should show here:</p>
+
+                    <h5>{this.state.user.firstName}</h5>
+                    <h5>{this.state.user.lastName}</h5>
+
+                    <button onClick={this.handleToggleEditForm}>Edit User</button>
 
 
 
 
-                <Link to='/'>
-                    <button type="button">Back to Users</button>
-                </Link>
-            </div>
+
+                    <Link to='/'>
+                        <button type="button">Back to Users</button>
+                    </Link>
+                </div>
         )
     }
 }
